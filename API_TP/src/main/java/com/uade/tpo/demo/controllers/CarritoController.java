ac45c8c4e1dcc02310d3cdfaaec134a82f9aad64
mbiancatto <maximiliano.biancatto@outlook.com>
2026-04-16 19:18:51 -0300

    Se agrega CarritoRequest y CarritoService

---
+++ API_TP/src/main/java/com/uade/tpo/demo/controllers/CarritoController.java
@@ -0,0 +1,15 @@
+package com.uade.tpo.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carrito")
+public class CarritoController {
+
+    @Autowired
+    private CarritoService
+
+    
+}
