0394d7adb425fb2fa387e43d165ac8e4aa213fb0
CrusesRamiro <crusesramiro@gmail.com>
2026-04-16 20:19:57 -0300

    carrito service and implementation.2

--- API_TP/src/main/java/com/uade/tpo/demo/service/CarritoService.java
+++ API_TP/src/main/java/com/uade/tpo/demo/service/CarritoService.java
@@ -6,13 +6,25 @@ public interface CarritoService {
 
     Carrito getCarritoByUsuarioId(Long usuarioId);
 
+    // Trae el carrito, o lo crea si no existe
+
     Carrito agregarItem(Long usuarioId, Long itemId, Integer cantidad);
+
+    // Agrega un item o suma cantidad ya existe
 
     Carrito actualizarCantidad(Long usuarioId, Long itemId, Integer cantidad);
 
+    // Actualiza la cantidad de un item, si es 0 lo borra
+
     void eliminarItem(Long usuarioId, Long itemId);
+
+    // Elimina un item del carrito
 
     void vaciarCarrito(Long usuarioId);
 
+    //Elimina todos ! los items del carrito
+
     double calcularTotal(Long usuarioId);
+
+    // Calcula el total de los items del carrito
 }
