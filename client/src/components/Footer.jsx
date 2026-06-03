afd528b348a0d92765a9b78e119c7515ddc3cc75
TomasRicoy <tricoy.uade@gmail.com>
2026-06-03 15:15:58 -0300

    mejoras esteticas footer agrega grupo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p><strong>Resonica</strong> - Maqueta de Frontend</p>
+      <p><strong>Resonica</strong> - Maqueta de Frontend - Grupo 1</p>
       <p style={{ marginTop: '0.3rem' }}>Integrantes: Nayla Malena Mongelos, Ramiro Pedro Cruses, Maximiliano Biancatto, Tomas Agustin Ricoy, Ivan Damianoff</p>
     </footer>
   )
